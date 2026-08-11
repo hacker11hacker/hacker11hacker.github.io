a2d101859ae213f290addb2d65d2aec812649c39
Hacker11 <shlomoheimm@gmail.com>
2026-08-11 07:17:44 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,11 @@
+const CONFIG = {
+  siteName: "the hacker",
+  siteTagline: "הפרויקטים שלי",
+  logoUrl: "https://i.ibb.co/sJbYj94K/hacker-rounded-2.png",
+
+  spreadsheetId: "1AYu9_Xihuwn-ehKQgHix9NuSutZFbLLCRWQ8pjpbZHQ",
+  projectsSheetName: "גיליון1",
+  profilesSheetName: "גיליון2",
+  sheetCsvUrl: "",
+  profilesCsvUrl: "",
+};
